2549d7db7cb8febed47ad91682363aefd5c727d5
eleztian <eleztian@gmail.com>
2017-03-16 22:46:19 +0800

    词法测试文件

---
+++ logic/testfile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*a111*/
+/**/#define s = s
+//b222
+void main()/*sdsd
+*/{
+    int a = 3 /2 ;
+    char b = 'c';
+    //c333
+    printf("hello world/*555*/  \n");/*44d4
+    444
+
+    */
+}
