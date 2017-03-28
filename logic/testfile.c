d2c08fc65dd71fe7c6c4ced3eaeacaf457e70b0b
eleztian <eleztian@gmail.com>
2017-03-28 23:43:05 +0800

    修改了一个BUG

--- logic/testfile.c
+++ logic/testfile.c
@@ -1,8 +1,12 @@
 int a(int b){
     if (a>3){
+     while(3);
         if (c>3){
-            if (d >5){}
+            if (d >5|| d>7){
+             while(3);}
+             while(3);
         }
+        while(3);
         while(3);
     }
     while(1);
